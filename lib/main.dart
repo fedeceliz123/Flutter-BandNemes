8772381b2a89d3dea3fdafd6c54246c2c53820f3
unknown <fede_celiz92@hotmail.com>
2022-01-12 15:50:17 -0300

    flutter 1

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+//importar paginas
+import 'package:band_names/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      initialRoute: 'home',
+      routes: {'home': (_) => HomePage()},
+    );
+  }
+}
